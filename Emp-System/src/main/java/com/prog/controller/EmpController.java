18fe6aa6f73bb2074b51aa91578c43bd2ab28241
praveen5630978 <130634422+praveen5630978@users.noreply.github.com>
2023-04-13 11:24:20 +0530

    Add files via upload

---
+++ Emp-System/src/main/java/com/prog/controller/EmpController.java
@@ -0,0 +1,82 @@
+package com.prog.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.prog.entity.Employee;
+import com.prog.service.EmpService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class EmpController {
+	
+	@Autowired
+private EmpService service;
+	
+	@GetMapping("/")
+	public String home(Model m) {
+		
+		List<Employee> emp=service.getAllEmp();
+		m.addAttribute("emp",emp);
+		
+		return "index";
+	}
+	
+	@GetMapping("/addemp")
+	public String addemp() {
+		return "add_emp";
+	}
+	
+	@PostMapping("/register")
+	public String empRegister(@ModelAttribute Employee e, HttpSession session) {
+		
+		System.out.println(e);
+		
+		service.addEmp(e);
+		session.setAttribute("msg", "Employee added succesfully..");
+		
+		return "redirect:/";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable int id, Model m) {
+		
+		Employee e=service.getEmpById(id);
+		m.addAttribute("emp",e);
+		
+		return "edit";
+	}
+	
+	@PostMapping("/update")
+	
+	public String updateEmp(@ModelAttribute Employee e, HttpSession session)
+{
+ service.addEmp(e);
+ session.setAttribute("msg","Emp data update successful");
+ return "redirect:/";
+ 
+}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteEmp(@PathVariable int id, HttpSession session)
+	{
+		service.deleteEmp(id);
+		session.setAttribute("msg", "Emp data delete succesful");
+		return "redirect:/";
+		
+	}	
+	
+}
+
+
+
+
+
+
